6fea28c77aa43e832d4d1160f082f61ada1defce
Serhey1992 <makss92i@gmail.com>
2022-11-26 17:01:59 +0200

    1

---
+++ js/task-01.js
@@ -0,0 +1,22 @@
+const categoriesListEl = document.querySelector("ul")
+// console.log(categoriesListEl);
+const numberOfCategories = categoriesListEl.querySelectorAll(".item");
+console.log("Number of categories:", numberOfCategories.length);
+
+const firstItem = numberOfCategories[0];
+const firstItemTitle = firstItem.querySelector("h2")
+console.log("Category:",firstItemTitle.textContent);
+const elementsOfFirstItem = firstItem.querySelectorAll("li");
+console.log("Elements:", elementsOfFirstItem.length);
+
+const secondItem = numberOfCategories[1];;
+const secondItemTitle = secondItem.querySelector("h2")
+console.log("Category:",secondItemTitle.textContent);
+const elementsOfSecondItem = secondItem.querySelectorAll("li");
+console.log("Elements:", elementsOfSecondItem.length);
+
+const lastItem = numberOfCategories[2];;
+const lastItemTitle = lastItem.querySelector("h2")
+console.log("Category:",lastItemTitle.textContent);
+const elementsOfLastItem = lastItem.querySelectorAll("li");
+console.log("Elements:", elementsOfLastItem.length);
