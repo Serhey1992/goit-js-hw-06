0f8d4624905984e7a4b2336932e2fc5368a2ab35
Serhey1992 <makss92i@gmail.com>
2022-12-02 23:34:19 +0200

    9+

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const body = document.querySelector("body");
+const colorBtn = document.querySelector(".change-color");
+const colorValue = document.querySelector(".color");
+
+
+colorBtn.addEventListener("click", () =>{
+  let colorChange = getRandomHexColor();
+  body.style.backgroundColor = colorChange;
+  colorValue.textContent = colorChange;
+});
