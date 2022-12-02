cdcb16f4edf54a5cb8387219b6c008585ebc4e6c
Serhey1992 <makss92i@gmail.com>
2022-12-02 19:39:17 +0200

    8+

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const formEl = document.querySelector(".login-form");
+const formInformation = {};
+formEl.addEventListener("submit", onFormSubmit);
 
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    const {
+        elements: { email, password }
+    } = evt.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        return alert("Заповнені не усі поля!");
+    } else {
+        formInformation.email = `${email.value}`
+        formInformation.password = `${password.value}`
+    }
+ console.log(formInformation);
+  evt.currentTarget.reset();
+}
